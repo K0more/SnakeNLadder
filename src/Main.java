2490ab1af4b7de53df50a11f69905e27de956903
K0more <arch.phenon@gmail.com>
2024-02-03 01:12:05 +0530

    UC1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int position=0;
+        Player p1 = new Player(position);
+
+        System.out.println("Position of the player 1: "+ p1.currentPosition);
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,8 @@
+public class Player {
+    int currentPosition;
+
+    Player(int position){
+        this.currentPosition=position;
+    }
+
+}
